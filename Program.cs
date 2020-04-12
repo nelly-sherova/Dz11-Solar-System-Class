ff176052ee755184382af906c8d38de11c59826d
unknown <lvova.nikol@mail.ru>
2020-04-12 11:54:37 +0300

    Создание класса, его полей  и его конструкторов

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 
 namespace SolarSystem
 {
+    class SolarSystem 
+    {
+        public string planetName;
+        public double diametrOfPlanet;
+        public double massOfPlanet;
+        public double distanceOfThePlanetFromTheSun;
+        public SolarSystem(){}
+        public SolarSystem(string a, double b, double c, double d)
+        {
+            this.planetName = a;
+            this.diametrOfPlanet = b;
+            this.massOfPlanet = c;
+            this.distanceOfThePlanetFromTheSun = d;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
